0ac6083eade0a46bed4cd1200c89c4eec7f6a6f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-18 17:50:34 +0000

    Deploy Smithay/client-toolkit to github.com/Smithay/client-toolkit.git:gh-pages

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["smithay_client_toolkit"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"smithay_client_toolkit/output/struct.Mode.html\" title=\"struct smithay_client_toolkit::output::Mode\">Mode</a>",synthetic:false,types:["smithay_client_toolkit::output::Mode"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"smithay_client_toolkit/seat/keyboard/struct.ModifiersState.html\" title=\"struct smithay_client_toolkit::seat::keyboard::ModifiersState\">ModifiersState</a>",synthetic:false,types:["smithay_client_toolkit::seat::keyboard::state::ModifiersState"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
